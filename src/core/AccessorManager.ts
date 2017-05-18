5b9899decd4c40834900373a874e56c27ba543ea
jai <jaikant@gmail.com>
2017-05-18 17:04:26 +0530

    commit update

--- src/core/AccessorManager.ts
+++ src/core/AccessorManager.ts
@@ -46,11 +46,7 @@ export class AccessorManager {
   add(accessor){
     if(accessor instanceof StatefulAccessor){
       if(accessor instanceof BaseQueryAccessor && accessor.key == "q"){
-        if(this.queryAccessor !== noopQueryAccessor){
-          throw new Error("Only a single instance of BaseQueryAccessor is allowed")
-        } else {
           this.queryAccessor = accessor
-        }
       }
       let existingAccessor = this.statefulAccessors[accessor.key]
       if(existingAccessor){
